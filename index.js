b373892beb7c3391484dfcefd6c298e475e9e3e0
petrec <petrec@coventry.ac.uk>
2019-10-04 18:35:42 +0000

    Pull airbnb linting config.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const Koa = require('koa')
+const Koa = require('koa');
 
-const router = require('./routes/router.js')
+const router = require('./routes/router.js');
 
-const app = new Koa()
+const app = new Koa();
 
-app.use(router.routes())
+app.use(router.routes());
 
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Server running on port ${port}.`))
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}.`));
