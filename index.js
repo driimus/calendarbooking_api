a252de6b946ed382a2144de6a3c1a3c5eac5bf14
petrec <petrec@coventry.ac.uk>
2019-11-16 14:57:57 +0000

    Fix linting errors in root file.

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ const app = new Koa();
 
 app.use(router.routes());
 
-// Cache all the custom models.
-const models = {
-  User: require('./modules/User'),
-  Calendar: require('./modules/Calendar'),
-  Activity: require('./modules/Activity'),
-  Comment: require('./modules/Comment'),
-  Tag: require('./modules/Tag')
-};
-
 const Connection = require('./db');
+
+// Cache all the custom models.
+const User = require('./modules/User');
+const Calendar = require('./modules/Calendar');
+const Activity = require('./modules/Activity');
+const Comment = require('./modules/Comment');
+const Tag = require('./modules/Tag');
+
+const models = [User, Calendar, Activity, Comment, Tag];
 
 const port = process.env.PORT || 3000;
 app.listen(port, async () => {
   // Initialize a new pool.
   const pool = new Connection();
   // Update the database schema.
-  Promise.all(Object.values(models).map((model) => pool.query(model.schema)));
+  Promise.all(models.map((model) => pool.query(model.schema)));
   // Drain the pool of connections.
   await pool.end();
   console.log(`Server running on port ${port}.`);
