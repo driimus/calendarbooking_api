b581ceee8255898ebc88a4e36c3c1c1b276ca735
petrec <petrec@coventry.ac.uk>
2019-11-16 02:05:30 +0000

    Updated database wrapper.

--- db/db.js
+++
@@ -1,8 +0,0 @@
-
-const { Pool } = require('pg');
-
-const environment = process.env.NODE_ENV || 'development';
-const config = require('./config.js')[environment];
-
-const db = new Pool(config);
-module.exports = db;
