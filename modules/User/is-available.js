58c5cf60767cbd7666508646dd559446871cb144
rosariot <rosariot@coventry.ac.uk>
2019-11-16 15:12:55 +0000

    Implemented username availability check.

---
+++ modules/User/is-available.js
@@ -0,0 +1,31 @@
+
+// User arrtibutes for which availability checks are enabled.
+const FIELDS = {
+  username: (value) => {
+    // Usernames must be a non-null string.
+    if (typeof value !== 'string' || value.length === 0) throw new Error('missing username');
+  },
+};
+
+/**
+ * Checks whether the value is available for a given user attribute.
+ *
+ * @async
+ * @param {string} field - Name of the user attribute.
+ * @param value - Value to be searched for.
+ */
+async function isAvailable(field, value) {
+  if (Object.keys(FIELDS).includes(field) === false) throw new Error(`invalid field "${field}"`);
+  // Validate the given value.
+  FIELDS[field](value);
+  // Check that the username is not taken.
+  const sql = `SELECT id FROM users WHERE ${field}=$1 LIMIT 1`;
+  const { rows: { length: exists } } = await this.db.query(sql, [value]);
+  if (exists !== 0) throw new Error(`${field} "${value}" already in use`);
+  return true;
+}
+
+module.exports = (User) => {
+  User.prototype.isAvailable = isAvailable;
+  return true;
+};
