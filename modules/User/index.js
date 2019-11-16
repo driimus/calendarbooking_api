e6205491173c8c369c68fc82c46e1b2a1f8f7308
petrec <petrec@coventry.ac.uk>
2019-11-16 15:05:33 +0000

    Fixed linting errors for models.

--- modules/User/index.js
+++ modules/User/index.js
@@ -1,16 +1,17 @@
-'use strict';
-
-const db = require('../../db');
+const Connection = require('../../db');
 
 class User {
   constructor() {
     return (async () => {
-      this.db = new db();
+      this.db = new Connection();
       return this;
     })();
   }
 }
 
+// Extend base class
 require('./schema')(User);
+require('./is-available')(User);
+require('./register')(User);
 
 module.exports = User;
