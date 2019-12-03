78231c90be4da118e79100eba98a60e32da636f4
petrec <petrec@coventry.ac.uk>
2019-12-03 15:28:12 +0000

    Removed entity relations for test environment

--- modules/Comment/schema.js
+++ modules/Comment/schema.js
@@ -1,8 +1,8 @@
 module.exports = (Comment) => {
   Comment.schema = `CREATE TABLE IF NOT EXISTS comment(
     id SERIAL PRIMARY KEY,
-    userId INT REFERENCES users(id),
-    activityId INT REFERENCES activity(id),
+    userId INT ${process.env.NODE_ENV === 'test' ? '' : 'REFERENCES users(id)'},
+    activityId INT ${process.env.NODE_ENV === 'test' ? '' : 'REFERENCES activity(id)'},
     allText TEXT NOT NULL,
     dateCreated TIMESTAMPTZ DEFAULT now(),
     dateModified TIMESTAMPTZ
