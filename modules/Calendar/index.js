e77254465ab0489d67d64ef556bca55a73f9e057
petrec <petrec@coventry.ac.uk>
2019-12-03 18:23:30 +0000

    Implemented calendar event creation

--- modules/Calendar/index.js
+++ modules/Calendar/index.js
@@ -4,11 +4,14 @@ class Calendar {
   constructor() {
     return (async () => {
       this.db = new Connection();
+      await this.db.query(Calendar.schema);
       return this;
     })();
   }
 }
 
 require('./schema')(Calendar);
+require('./create')(Calendar);
+require('./is-valid')(Calendar);
 
 module.exports = Calendar;
