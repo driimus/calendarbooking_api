5cbb40f7eb5873823e243288e8c1176fae96e066
petrec <petrec@coventry.ac.uk>
2019-12-09 11:30:06 +0000

    Updated event getter to retrieve activtiy info

--- modules/Calendar/get-all.js
+++ modules/Calendar/get-all.js
@@ -11,9 +11,22 @@ const { utils: { isId } } = require('../Utils');
  */
 async function getAll(userId) {
   const id = await isId(userId, 'User');
-  const sql = 'SELECT * FROM calendar WHERE user_id=$1';
-  const { rows: events } = await this.db.query(sql, [id]);
-  if (events.length === 0) throw new Error(`No events found for user ID "${id}"`);
+  const sql = `SELECT * FROM calendar
+    INNER JOIN activity ON calendar.activity_id=activity.id
+    WHERE user_id=$1
+  `;
+  const { rows } = await this.db.query(sql, [id]);
+  if (rows.length === 0) throw new Error(`No events found for user ID "${id}"`);
+  const events = rows.map((row) => ({
+    id: row.id,
+    userId: row.user_id,
+    activityId: row.activity_id,
+    start: new Date(row.from_ts),
+    end: new Date(row.to_ts),
+    title: row.title,
+    description: row.description,
+    url: row.url,
+  }));
   return events;
 }
 
